8eeff0025e884ed997c2221acf3125b467dbba88
MACARENASTORE <oclusiva9204@gmail.com>
2024-07-30 12:15:04 -0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,138 @@
+import Layout from "@/components/Layout";
+import Link from "next/link";
+
+import { skills, experiences, projects } from "@/pages/profile";
+
+export default function Home() {
+  return (
+    <Layout footer={true}>
+      {/* Header Card */}
+      <header className="row">
+        <div className="col-md-12">
+          <div className="card card-body bg-secondary text-light animate__animated animate__fadeIn">
+            <div className="row">
+              <div className="col-md-4 text-center">
+                <img
+                  src="/profile_diego.png"
+                  alt="profile Diego Barreto"
+                  className="img-fluid"
+                />
+              </div>
+              <div className="col-md-8">
+                <h1>Diego Fernando Barreto</h1>
+                <h3>FullStack Developer</h3>
+                <p>
+                  Soy un Junior Full Stack Developer apasionado por la
+                  tecnología y el desarrollo de software. Me especializo en el
+                  desarrollo tanto de frontend como de backend, y disfruto
+                  creando aplicaciones web intuitivas y funcionales. Estoy
+                  constantemente aprendiendo y mejorando mis habilidades para
+                  mantenerme al día con las últimas tendencias y tecnologías del
+                  desarrollo de software.
+                </p>
+                <Link href="/hireme" className="btn btn-outline-light">
+                  Hire Me
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      {/* Second section */}
+
+      <section className="row">
+        <div className="col-md-4 py-2">
+          <div className="card bg-light animate__animated animate__fadeInLeft">
+            <div className="card-body">
+              <h1>Skills</h1>
+
+              {/* Skill Progress  */}
+              {skills.map(({ skill, percentage }, i) => (
+                <div className="py-3" key={i}>
+                  <h5>{skill}</h5>
+                  <div className="progress ">
+                    <div
+                      className="progress-bar"
+                      role="progressbar"
+                      style={{ width: `${percentage}%` }}
+                      aria-valuenow="50"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    ></div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-8 py-2">
+          {/* Experience */}
+          <div className="card bg-light animate__animated animate__fadeInRight">
+            <div className="card-body">
+              <h1>Experience</h1>
+
+              <ul>
+                {/* List Item Experience */}
+                {experiences.map(({ title, description, from, to }, index) => (
+                  <li key={index}>
+                    <h3>{title}</h3>
+                    <h5>
+                      {from} {to ? `- ${to}` : "- current"}
+                    </h5>
+                    <p>{description}</p>
+                  </li>
+                ))}
+              </ul>
+              <Link href="/hireme" className="btn btn-light">
+                Know More
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Porfolio */}
+      <section>
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card card-body bg-dark">
+              <div className="row">
+                <div className="col-md-12 my-2">
+                  <h1 className="text-center text-light">Portfolio</h1>
+                </div>
+                {projects.map(({ name, description, image }, index) => (
+                  <div className="col-md-4 p-2" key={index}>
+                    <div className="card h-100">
+                      <div className="overflow">
+                        <img
+                          src={`/${image}`}
+                          alt="project"
+                          className="card-img-top"
+                        />
+                      </div>
+                      <div className="card-body">
+                        <h3>{name}</h3>
+                        <p>{description}</p>
+                        <a href="#!">Know More</a>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+
+                <div className="col-md-12 mt-4">
+                  <div className="text-center">
+                    <Link href="/portfolio" className="btn btn-outline-light">
+                      More Projects
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
+}
---
+++ src/pages/profile.js
@@ -0,0 +1,182 @@
+export const skills = [
+  {
+    skill: "JavaScript",
+    percentage: 80, // Ajustado según tu nivel de experiencia
+  },
+  {
+    skill: "TypeScript",
+    percentage: 70, // Basado en tu capacitación
+  },
+  {
+    skill: "Node.js",
+    percentage: 75, // Basado en tu formación en desarrollo back end
+  },
+  {
+    skill: "React.js",
+    percentage: 85, // Basado en tus proyectos y capacitación en frontend
+  },
+  {
+    skill: "HTML & CSS",
+    percentage: 85, // Habilidad esencial para frontend
+  },
+  {
+    skill: "Bootstrap",
+    percentage: 80, // Basado en tu experiencia y formación
+  },
+  {
+    skill: "SQL & NoSQL Databases",
+    percentage: 70, // Incluye experiencia en bases de datos relacionales y no relacionales
+  },
+  {
+    skill: "API Integration",
+    percentage: 75, // Habilidad desarrollada a través de tus proyectos
+  },
+];
+export const experiences = [
+  {
+    title: "Diplomado en Desarrollo Back End - Nivel Intermedio",
+    description:
+      "Adquirí habilidades avanzadas en desarrollo de back end, incluyendo la creación de APIs, gestión de bases de datos y arquitectura de aplicaciones. Este diplomado proporcionó una comprensión profunda de las tecnologías y mejores prácticas en el desarrollo de back end.",
+    institution: "Universidad Distrital Francisco Jose De Caldas",
+    status: "Certificado",
+    certificationDate: "Agosto de 2023",
+    durationHours: 125,
+  },
+  {
+    title: "Diplomado en Repositorio de Software",
+    description:
+      "Desarrollé conocimientos en gestión y uso de repositorios de software, incluyendo control de versiones con herramientas como Git y GitHub. El diplomado incluyó prácticas en colaboración en proyectos de código y mantenimiento de repositorios.",
+    institution: "Universidad Distrital Francisco Jose De Caldas",
+    status: "Certificado",
+    certificationDate: "Agosto de 2023",
+    durationHours: 48,
+  },
+  {
+    title: "Diplomado en Fundamentos de Programación Frontend",
+    description:
+      "Estudié los fundamentos del desarrollo frontend, incluyendo HTML, CSS y JavaScript. Aprendí a construir interfaces de usuario responsivas y a integrar con backends para crear aplicaciones web completas.",
+    institution: "Universidad Distrital Francisco Jose De Caldas",
+    status: "Certificado",
+    certificationDate: "Agosto de 2023",
+    durationHours: 48,
+  },
+  {
+    title: "Diplomado en Programación TypeScript y JavaScript",
+    description:
+      "Me especialicé en TypeScript y JavaScript, aprendiendo a escribir código robusto y escalable. Este diplomado incluyó prácticas en desarrollo de aplicaciones web usando frameworks y bibliotecas modernas.",
+    institution: "Universidad Distrital Francisco Jose De Caldas",
+    status: "Certificado",
+    certificationDate: "Agosto de 2023",
+    durationHours: 48,
+  },
+  {
+    title: "Diplomado en Programación con Énfasis en Desarrollo Web",
+    description:
+      "Completé un extenso diplomado en desarrollo web, cubriendo tanto el desarrollo frontend como backend. El curso incluyó prácticas en diseño de interfaces, desarrollo de aplicaciones web y arquitectura de software.",
+    institution: "Universidad Tecnológica de Pereira",
+    status: "Certificado",
+    certificationDate: "Diciembre de 2022",
+    durationHours: 320,
+  },
+  {
+    title: "Diplomado en Habilidades para los Negocios",
+    description:
+      "Desarrollé habilidades en gestión de proyectos y procesos de negocio, incluyendo la optimización de operaciones y el análisis de datos. El diplomado ayudó a mejorar mi capacidad para gestionar equipos y proyectos de manera efectiva.",
+    institution: "Universidad San Buenaventura",
+    status: "Certificado",
+    certificationDate: "Diciembre de 2020",
+    durationHours: 320,
+  },
+];
+
+export const projects = [
+  {
+    name: "Galería de Imágenes de Juegos de Game Pass",
+    description:
+      "Desarrollé una galería de imágenes para juegos disponibles en Game Pass utilizando React. La aplicación permite a los usuarios navegar por una colección de imágenes de diferentes juegos. Implementé una interfaz intuitiva y responsiva, y utilicé React Hooks para la gestión del estado, mejorando la experiencia del usuario y la eficiencia de la aplicación.",
+    image: "gamepassgallery.png",
+  },
+  {
+    name: "Aplicación de Solicitudes a una API",
+    description:
+      "Creé una aplicación web que realiza solicitudes a una API externa y muestra los resultados de manera dinámica. Utilicé React para el frontend y Fetch API para realizar las solicitudes. La aplicación maneja los errores de red de forma eficiente y muestra mensajes informativos a los usuarios, garantizando una experiencia de usuario robusta.",
+    image: "apirequestsapp.png",
+  },
+  {
+    name: "Blog Personal",
+    description:
+      "Desarrollé un blog personal con Next.js, permitiendo la publicación de contenido mediante Markdown. La aplicación soporta la creación de artículos, comentarios y etiquetas. Implementé un sistema de autenticación basado en OAuth para permitir el inicio de sesión con cuentas de Google y GitHub, garantizando la seguridad y facilidad de acceso para los usuarios.",
+    image: "personalblog.png",
+  },
+  {
+    name: "Portfolio Personal",
+    description:
+      "Diseñé y desarrollé mi portfolio personal utilizando HTML, CSS y JavaScript. El sitio web muestra mis proyectos, habilidades y experiencia laboral. Implementé animaciones CSS para mejorar la interactividad y la experiencia del usuario, destacando mis habilidades en diseño frontend y atención al detalle.",
+    image: "personalportfolio.png",
+  },
+];
+
+export const posts = [
+  {
+    title: "React",
+    content:
+      "React es una biblioteca de JavaScript para construir interfaces de usuario. Es mantenida por Facebook y permite crear aplicaciones web dinámicas y responsivas. Con React, puedes construir componentes reutilizables que gestionan su propio estado.",
+    imageURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "Angular",
+    content:
+      "Angular es un framework de desarrollo web front-end basado en TypeScript, mantenido por Google. Ofrece una solución completa para construir aplicaciones web robustas y escalables con un enfoque en la modularidad y la reutilización del código.",
+    imageURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "Next.js",
+    content:
+      "Next.js es un framework de desarrollo web que permite la creación de aplicaciones React con funcionalidades como el renderizado del lado del servidor y la generación de sitios estáticos. Facilita la optimización del rendimiento y la mejora de la experiencia del desarrollador.",
+    imageURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "Spring Boot",
+    content:
+      "Spring Boot es un framework de desarrollo backend para Java que facilita la creación de aplicaciones independientes, productivas y basadas en Spring. Proporciona configuraciones predeterminadas para proyectos, lo que simplifica el desarrollo y despliegue de aplicaciones.",
+    imageURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "Node.js",
+    content:
+      "Node.js es un entorno de ejecución de JavaScript del lado del servidor. Permite construir aplicaciones escalables y eficientes utilizando un modelo de E/S sin bloqueo. Con Node.js, puedes usar JavaScript tanto en el frontend como en el backend.",
+    imageURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "PostgreSQL",
+    content:
+      "PostgreSQL es un sistema de gestión de bases de datos relacional y de código abierto. Es conocido por su estabilidad, escalabilidad y soporte avanzado para procedimientos almacenados y transacciones complejas.",
+    imageURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "MongoDB",
+    content:
+      "MongoDB es una base de datos NoSQL orientada a documentos que utiliza documentos similares a JSON con esquemas dinámicos. Es ideal para aplicaciones que manejan grandes volúmenes de datos no estructurados.",
+    imageURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "Docker",
+    content:
+      "Docker es una plataforma de contenedorización que permite crear, desplegar y ejecutar aplicaciones en contenedores. Facilita la portabilidad y escalabilidad de las aplicaciones, asegurando que se ejecuten de manera consistente en diferentes entornos.",
+    imageURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "Git",
+    content:
+      "Git es un sistema de control de versiones distribuido que permite a los desarrolladores rastrear cambios en el código fuente y colaborar en proyectos de manera eficiente. Es una herramienta esencial para el desarrollo de software moderno.",
+    imageURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  }
+  
+];
